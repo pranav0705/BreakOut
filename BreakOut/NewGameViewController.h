16d2ddcea1cae9e6b2ac2ac543d305da8a4d1373
Pranav Bhandari <Pranav@Pranavs-Mac.local>
2016-02-15 22:57:14 -0500

    Committed

---
+++ BreakOut/NewGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewGameViewController.h
+//  BreakOut
+//
+//  Created by Pranav Bhandari on 2/11/16.
+//  Copyright © 2016 Pranav Bhandari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewGameViewController : UIViewController
+
+@end
---
+++ BreakOut/NewGameViewController.m
@@ -0,0 +1,37 @@
+//
+//  NewGameViewController.m
+//  BreakOut
+//
+//  Created by Pranav Bhandari on 2/11/16.
+//  Copyright © 2016 Pranav Bhandari. All rights reserved.
+//
+
+#import "NewGameViewController.h"
+
+@interface NewGameViewController ()
+
+@end
+
+@implementation NewGameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
